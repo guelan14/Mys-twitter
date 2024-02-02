d679801e199f1f7f67a70ad965f4381fe3b38e14
Miguel Angel Neumann <miguelangelneumann@gmail.com>
2024-02-02 17:09:57 -0300

    arreglado publication

--- api/controllers/publication.js
+++ api/controllers/publication.js
@@ -5,44 +5,43 @@ const fs = require("fs");
 const jwt = require("jsonwebtoken");
 
 const save = async (req, res) => {
+  const description = req.body.description;
+  const imageFile = req.file;
+  let image = "";
   //obtain Data from client
-  if (!req.file) {
-    return res.status(404).send({
-      status: "error",
-      message: "Peticion no incluye imagen",
-    });
-  }
+  if (imageFile) {
+    image = "pub-" + req.file.originalname;
+    let imageSplit = image.split(".");
+    const extension = imageSplit[1];
 
-  let description = req.body.description;
+    if (extension != "png" && extension != "jpg" && extension != "gif") {
+      const filePath = req.file.path;
+      const fileDeleted = fs.unlinkSync(filePath);
 
-  let image = "pub-" + req.file.originalname;
-  let imageSplit = image.split(".");
-  const extension = imageSplit[1];
-
-  if (extension != "png" && extension != "jpg" && extension != "gif") {
-    const filePath = req.file.path;
-    const fileDeleted = fs.unlinkSync(filePath);
-
-    return res.status(400).send({
-      status: "error",
-      message: "extension del fichero invalida",
-    });
+      return res.status(400).send({
+        status: "error",
+        message: "extension del fichero invalida",
+      });
+    }
   }
   //using user from token
-  const user = req.user.id;
+  const user_id = req.user.id;
+  const username = req.user.username;
 
   //create publication in db
   try {
     await pool.query(
-      "INSERT INTO publication (user_id, text_publication, image) VALUES (? , ?, ?)",
-      [user, description, image]
+      "INSERT INTO publication (user_id, text_publication, image, username) VALUES (? , ?, ?, ?)",
+      [user_id, description, image, username]
     );
     return res.status(200).send({
       status: "succes",
       message: "publication created",
     });
   } catch (error) {
-    return res.status(500).send({ status: "error", message: error.message });
+    return res
+      .status(500)
+      .send({ status: "error aca", message: error.message });
   }
 };
 
@@ -74,8 +73,7 @@ const list = async (req, res) => {
 
       return res.status(200).send({
         status: "succes",
-        message: "lista",
-        follows,
+        message: "lista de publicacion",
         publications,
       });
     }
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -10,8 +10,14 @@ const userTest = (req, res) => {
 };
 
 const register = async (req, res) => {
-  const { email, firstname, lastname, user_password, confirm_password } =
-    req.body;
+  const {
+    email,
+    firstname,
+    lastname,
+    user_password,
+    confirm_password,
+    username,
+  } = req.body;
 
   if (user_password !== confirm_password) {
     return res.status(500).send({
@@ -21,24 +27,36 @@ const register = async (req, res) => {
   }
 
   try {
-    const [results] = await pool.query(
+    const [resultsEmail] = await pool.query(
       "SELECT email FROM users WHERE email=?",
       [email]
     );
 
-    if (results.length > 0) {
+    const [resultsUsername] = await pool.query(
+      "SELECT email FROM users WHERE username=?",
+      [username]
+    );
+
+    if (resultsEmail.length > 0) {
       // email already registered
       return res.status(200).send({
         status: "error",
         message: "Email already registered",
       });
     }
+    if (resultsUsername.length > 0) {
+      // email already registered
+      return res.status(200).send({
+        status: "error",
+        message: "Username already registered",
+      });
+    }
 
-    const passCrypted = bcrypt.hashSync(user_password);
+    const passCrypted = bcrypt.hashSync(user_password, 10);
 
     await pool.query(
-      "INSERT INTO users (email, firstname, lastname, user_password) VALUES (?, ?, ?, ?)",
-      [email, firstname, lastname, passCrypted]
+      "INSERT INTO users (email, firstname, lastname, user_password, username) VALUES (?, ?, ?, ?, ?)",
+      [email, firstname, lastname, passCrypted, username]
     );
     return res.status(200).send({
       status: "success",
@@ -80,6 +98,7 @@ const login = async (req, res) => {
       //create user object to pass
       const userForToken = {
         id: user[0].user_id,
+        username: user[0].username,
         iat: moment().unix(),
         exp: moment().add(1, "days").unix(),
       };
@@ -89,6 +108,7 @@ const login = async (req, res) => {
       return res.status(200).send({
         status: "success",
         message: "login successful",
+        username: user[0].username,
         token: token,
       });
     } else {
